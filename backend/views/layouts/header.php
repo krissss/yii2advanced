8f3786e8155ccb7f74e9348cd89ccfc14d2a094a
kriss <462679766@qq.com>
2017-11-17 20:01:50 +0800

    fix：menu push

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -17,7 +17,7 @@ $admin = Yii::$app->user->identity;
 
     <nav class="navbar navbar-static-top" role="navigation">
 
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
 

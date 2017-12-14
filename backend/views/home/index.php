ff54e2bff2444d656e44c94ffedb3688c01939b2
kriss <462679766@qq.com>
2017-12-14 09:37:41 +0800

    fix: title

--- backend/views/home/index.php
+++ backend/views/home/index.php
@@ -1,7 +1,9 @@
 <?php
 /** @var $this \yii\web\View */
 
+$this->title = '首页';
+
 /** @var \common\models\Admin $user */
 $user = Yii::$app->user->identity;
 
-echo '<h1 class="text-center">欢迎：'.$user->name.'</h1>';
+echo '<h1 class="text-center">欢迎：' . $user->name . '</h1>';

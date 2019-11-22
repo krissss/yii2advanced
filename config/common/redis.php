2f0b759f217a249881a89a200923ca740a4e568b
kriss <462679766@qq.com>
2019-11-22 16:35:35 +0800

    feat: redis remove db0 default

--- config/common/redis.php
+++ config/common/redis.php
@@ -5,7 +5,7 @@ $redisConf = [
     'class' => 'yii\redis\Connection',
     'hostname' => getenv('RDB_HOST'),
     'port' => getenv('RDB_PORT'),
-    'database' => 0,
+    //'database' => 0,
 ];
 if ($password) {
     $redisConf['password'] = $password;
